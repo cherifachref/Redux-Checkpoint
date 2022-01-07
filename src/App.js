3cba988e2078f9a1c3b61eb7090010e2a543c28f
Cherif Achref <bargo0089@gmail.com>
2022-01-07 05:34:26 +0100

    componentreact

--- src/App.js
+++ src/App.js
@@ -1,32 +1,18 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
+import Address from './Component/Profile/Address';
+import FullName from './Component/Profile/FullName';
+import ProfilePhoto from './Component/Profile/ProfilePhoto';
 import './style.css'
-import Myimage from './myimage.jpg'
 
 
 
 function App() {
-  let name="abc"
   
   return (
     <div>
 
-<div style={{border:'solid 1px black',maxWidth:'100vw'}}>
-
-<h1 className='title red' >Your name here:{name}</h1>
-
-
-<br />
-
-<img src={Myimage} />
-
-<br />
-<img src='./myimage2.png' />     
-</div>
-<video width="320" height="240" controls>
-
-<source src="myVideo.mp4" type="video/mp4" />
-
-</video>
+      <ProfilePhoto/>
+      <FullName/>
+      <Address/>
     
     </div>
   );
---
+++ src/Component/Profile/ProfilePhoto.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ProfilePhoto() {
+    return (
+        <div>
+            <img src='./me.jpg' />
+        </div>
+    )
+}
+
+export default ProfilePhoto
