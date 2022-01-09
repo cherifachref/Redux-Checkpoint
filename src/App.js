d9f48f1be443c40500c6edb233fc6295bd5a62d5
Cherif Achref <bargo0089@gmail.com>
2022-01-09 16:36:18 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Profile from "./profile/Profile";
+
+import React, { Component } from 'react'
+import { Button } from 'react-bootstrap';
+import Card01 from './component/Card';
 
 
 
-function App() {
-  
-  return (
-    <>
-      
-      <div style={{display:'flex',justifyContent: 'space-evenly',flexWrap: 'wrap',backgroundColor:'#ECC37C'}}>
-      <Profile fullName="Achref Cherif" bio="Biobboo" profession="Etudiant" >https://s1.1zoom.me/b5050/596/Evening_Forests_Mountains_Firewatch_Campo_Santo_549147_1366x768.jpg</Profile>
-      <Profile fullName="Batman Cherif" bio="Biobboo" profession="Etudiant" >https://wallpaper.dog/large/20492370.jpg</Profile>
-      <Profile fullName="SuperMan Cherif" bio="Biobboo" profession="Etudiant" >https://www.wallpapertip.com/wmimgs/4-41271_minimal-artwork-pyramids-dark-night-wallpaper-retro-4k.jpg</Profile>
-      <Profile fullName="Spiderman Cherif" bio="Biobboo" profession="Etudiant" >https://www.setaswall.com/wp-content/uploads/2017/06/Village-Wallpapers-30-1366-x-768.jpg</Profile>
-      <Profile fullName="FantaMan Cherif" bio="Biobboo" profession="Etudiant" >https://wallpapercave.com/wp/wp7242558.jpg</Profile>
-      <Profile fullName="Zalman Cherif" bio="Biobboo" profession="Etudiant" >https://www.wallpaperkiss.com/wimg/b/103-1036998_big.jpg</Profile>
-      <Profile fullName="IronMan Cherif" bio="Biobboo" profession="Etudiant" >https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTLMXBdYitaRtZust4igl-Y4jRcpyTQ3UNwyg&usqp=CAU</Profile>
-      <Profile fullName="HitMan Cherif" bio="Biobboo" profession="Etudiant" >https://cdn.wallpapersafari.com/88/2/MlLVcW.jpg</Profile>
-      
+export default class App extends Component {
+  constructor(props){
+    super(props)
+    this.state={show:true}
+  }
+
+  render() {
+    return (
+      <div>
+        {this.state.show && <Card01/>}
+        <Button onClick={()=>this.setState({show:!this.state.show})}>SHOW/HIDE</Button>
+        
       </div>
-    </>
-  );
+    )
+  }
 }
 
-export default App;
+
